f7059087efc5a3f87f8f24d9d88179dcc984be2a
joonsukkang <joonsukkang@uchicago.edu>
2023-11-16 12:35:32 -0800

    Add roxygen header to document strsplit1()

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,12 @@
+#' Split a string
+#'
+#' @param x A character vector with one element.
+#' @param split What to split on.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
